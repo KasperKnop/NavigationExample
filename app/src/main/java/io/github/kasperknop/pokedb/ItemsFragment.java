165a91a20868d39cf28d207d1a4ed78623f5331a
Kasper Knop Rasmussen <kasperknop@gmail.com>
2021-03-12 10:56:18 +0100

    Updated dependencies

--- app/src/main/java/io/github/kasperknop/pokedb/ItemsFragment.java
+++ app/src/main/java/io/github/kasperknop/pokedb/ItemsFragment.java
@@ -11,6 +11,6 @@ import androidx.fragment.app.Fragment;
 public class ItemsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_items, container, false);
+        return inflater.inflate(R.layout.fragment_items, container, true);
     }
 }
